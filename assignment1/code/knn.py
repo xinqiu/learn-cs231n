7073809fc352b151ac134a19b91194817036203e
xinqiu <xinqiu.94@gmail.com>
2016-12-08 11:45:41 +0800

    完成assignment1

---
+++ assignment1/code/knn.py
@@ -0,0 +1,107 @@
+import random
+import numpy as np
+from cs231n.data_utils import load_CIFAR10
+import matplotlib.pyplot as plt
+from cs231n.classifiers import KNearestNeighbor
+import seaborn as sns
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# Load the raw CIFAR-10 data.
+cifar10_dir = '../cs231n/datasets/cifar-10-batches-py'
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+# As a sanity check, we print out the size of the training and test data.
+print 'Training data shape: ', X_train.shape
+print 'Training labels shape: ', y_train.shape
+print 'Test data shape: ', X_test.shape
+print 'Test labels shape: ', y_test.shape
+
+# Visualize some examples from the dataset.
+# We show a few examples of training images from each class.
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+num_classes = len(classes)
+samples_per_class = 7
+for y, cls in enumerate(classes):
+    idxs = np.flatnonzero(y_train == y)
+    idxs = np.random.choice(idxs, samples_per_class, replace=False)
+    for i, idx in enumerate(idxs):
+        plt_idx = i * num_classes + y + 1
+        plt.subplot(samples_per_class, num_classes, plt_idx)
+        plt.imshow(X_train[idx].astype('uint8'))
+        plt.axis('off')
+        if i == 0:
+            plt.title(cls)
+# plt.show()
+
+# Subsample the data for more efficient code execution in this exercise
+num_training = 50000
+mask = range(num_training)
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+num_test = 5000
+mask = range(num_test)
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+print X_train.shape, X_test.shape
+
+classifier = KNearestNeighbor()
+classifier.train(X_train, y_train)
+
+dists = classifier.compute_distances_two_loops(X_test)
+print dists.shape
+
+# plt.imshow(dists, interpolation='none')
+# plt.show()
+
+
+y_test_pred = classifier.predict_labels(dists, k=1)
+
+# Compute and print the fraction of correctly predicted examples
+num_correct = np.sum(y_test_pred == y_test)
+accuracy = float(num_correct) / num_test
+print 'Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy)
+
+y_test_pred = classifier.predict_labels(dists, k=5)
+num_correct = np.sum(y_test_pred == y_test)
+accuracy = float(num_correct) / num_test
+print 'Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy)
+
+
+
+dists_one = classifier.compute_distances_one_loop(X_test)
+
+difference = np.linalg.norm(dists - dists_one, ord='fro')
+print 'Difference was: %f' % (difference, )
+if difference < 0.001:
+  print 'Good! The distance matrices are the same'
+else:
+  print 'Uh-oh! The distance matrices are different'
+
+dists_two = classifier.compute_distances_no_loops(X_test)
+
+# check that the distance matrix agrees with the one we computed before:
+difference = np.linalg.norm(dists - dists_two, ord='fro')
+print 'Difference was: %f' % (difference, )
+if difference < 0.001:
+  print 'Good! The distance matrices are the same'
+else:
+  print 'Uh-oh! The distance matrices are different'
+
+num_folds = 5
+k_choices = [1, 3, 5, 8, 10, 12, 15, 20, 50, 100]
+
+X_train_folds = []
+y_train_folds = []
+
+X_train_folds = np.array_split(X_train, num_folds)
+y_train_folds = np.array_split(y_train, num_folds)
+
+k_to_accuracies = {}
+
